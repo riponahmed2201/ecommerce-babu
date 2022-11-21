915779015193eee198e020bf1a4398c54a84ebd6
ripon <riponahmed2201@gmail.com>
2022-11-21 10:11:54 +0600

    backend mastering ok

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -9,9 +9,7 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="Logo" height="60" width="60">
-        </div>
+        @include('backend.layouts.preloader')
 
         <!-- Navbar -->
         @include('backend.layouts.navbar')
@@ -23,21 +21,7 @@
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
+            @include('backend.layouts.breadcrumb')
             <!-- /.content-header -->
 
             <!-- Main content -->
